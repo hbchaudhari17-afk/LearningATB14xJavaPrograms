fbbcfb3878a115d78be634d14cc69172bb828221
Harshal <hbchaudhari17@gmail.com>
2025-10-04 23:24:51 +0530

    Literals

---
+++ src/ex_03_Literals/Lab029_Literals.java
@@ -0,0 +1,17 @@
+package ex_03_Literals;
+
+public class Lab029_Literals {
+
+    public static void main(String[] args) {
+        int age = 65;
+        // Literal - integral
+
+     final float pi = ("3.14F");
+//        // Literal - float
+
+        final int girl age = 18;
+        //girl_age = 19;
+
+
+    }
+}
---
+++ src/ex_03_Literals/Lab_035_Non_Primitve_Literals.java
@@ -0,0 +1,8 @@
+package ex_03_Literals;
+
+public class Lab_035_Non_Primitve_Literals {
+    public static void main(String[] args) {
+        String name = "Pramod"; // String is bunch of char.
+        int[] arrays_of_items = new int[10];
+    }
+}

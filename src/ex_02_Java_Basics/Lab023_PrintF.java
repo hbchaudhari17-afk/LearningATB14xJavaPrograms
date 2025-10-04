be85d9bc2de6dfff0cbe449f76101968f280e065
Harshal <hbchaudhari17@gmail.com>
2025-10-04 15:51:47 +0530

    14th Sep 2025, all files added part 1

---
+++ src/ex_02_Java_Basics/Lab023_PrintF.java
@@ -0,0 +1,29 @@
+package ex_02_Java_Basics;
+
+public class Lab023_PrintF {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a);
+        System.out.println(b);
+      }
+    }
+
+//        System.out.println(a);
+//        System.out.println(b);
+
+//        System.out.print(a);
+//        System.out.print(b);
+
+
+
+
+
+
+    // %d -> int, byte, long, short, - data type
+    // %s -> String
+    // %f -> float, double,
+    // %b ->boolean
+
+
+
---
+++ src/ex_02_Java_Basics/Lab024_TableOfNine.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics;
+
+public class Lab024_TableOfNine {
+    public static void main(String[] args) {
+        System.out.println();
+        int table = 9;
+        System.out.printf("%d*1=%d", table, table*1);
+    }
+
+}

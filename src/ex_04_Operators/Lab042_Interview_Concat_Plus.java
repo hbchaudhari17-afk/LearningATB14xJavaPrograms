41f68cc1dec54017158f1f23e7e33fafebaa4843
Harshal <hbchaudhari17@gmail.com>
2025-10-06 00:28:16 +0530

    20th September Part 1 Operators

---
+++ src/ex_04_Operators/Lab042_Interview_Concat_Plus.java
@@ -0,0 +1,26 @@
+package ex_04_Operators;
+
+public class Lab042_Interview_Concat_Plus {
+    public static void main(String[] args) {
+        // + ->
+        int a = 10;
+        int b = 10;
+        System.out.println(a+b);
+
+        // + -> string -> Concatenation
+        String first_name = "Harshal";
+        String last_name = "Chaudhari";
+        System.out.println(first_name+last_name);
+
+ //       System.out.println(first_name-last_name);
+ //        System.out.println(first_name*last_name);
+ //        System.out.println(first_name/last_name);
+
+        // + -> behave differently with the data type.
+        // + -> operator overloading
+
+
+    }
+
+
+}
